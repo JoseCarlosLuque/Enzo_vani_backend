2c3fbbb7c987a6edca8d48bdd09d871c2e274aba
José Carlos Luque Castro <luquecastrojosecarlos@gmail.com>
2025-05-24 20:35:34 +0200

    Primera versión de mi api

--- Enzo_vani_app/main.py
+++ Enzo_vani_app/main.py
@@ -16,14 +16,20 @@ app.add_middleware(
 )
 
 # MongoDB connection
+# Con el server corriendo se puede ir a http://localhost:8000/docs, y testear los endpoints.
 client = MongoClient("mongodb://localhost:27017")
-db = client["tienda"]
-products_collection = db["products"]
-subscribers_collection = db["subscribers"]
+db = client["Enzo_vani"]
+products_collection = db["Products"]
+subscribers_collection = db["Subscribers"]
 
 # Schemas
 class Subscriber(BaseModel):
     email: EmailStr
+
+class Product(BaseModel):
+    name: str
+    price: float
+    stock: int
 
 @app.get("/products")
 def get_products():
@@ -31,11 +37,26 @@ def get_products():
     for product in products_collection.find():
         products.append({
             "id": str(product["_id"]),
-            "name": product["name"],
-            "price": product["price"],
+            "name": product["nombre"],
+            "price": product["precio"],
             "stock": product["stock"]
         })
     return products
+
+@app.post("/products")
+def add_product(product: Product):
+    result = products_collection.insert_one(product.dict())
+    return {"message": "Producto añadido", "product_id": str(result.inserted_id)}
+
+@app.get("/subscribers")
+def get_subscribers():
+    subscribers = []
+    for subscriber in subscribers_collection.find():
+        subscribers.append({
+            "id": str(subscriber["_id"]),
+            "email": subscriber["email"]
+        })
+    return subscriber
 
 @app.post("/subscribe")
 def subscribe(subscriber: Subscriber):
