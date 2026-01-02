41364c77d6244555ee64cb1387ffa7a1a3d340a1
José Carlos Luque Castro <luquecastrojosecarlos@gmail.com>
2026-01-02 08:35:44 +0100

    Arreglado problema rutas

--- Enzo_vani_app/auth.py
+++ Enzo_vani_app/auth.py
@@ -3,7 +3,7 @@ from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
-from database import users_collection
+from Enzo_vani_app.database import users_collection
 import config
 
 SECRET_KEY = config.auth_secret_key
